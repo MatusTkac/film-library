3c6941cb7b5e0ecf6688801a95385ba5ed4af92c
MatusTkac <matus.tkac7@gmail.com>
2026-03-19 09:56:25 +0100

    Initial commit: Creating Admin, AdminController, AdminService

---
+++ src/main/java/admin/AdminController.java
@@ -0,0 +1,6 @@
+package admin;
+
+public class AdminController {
+    private AdminService adminService;
+    private Admin admin;
+}
---
+++ src/main/java/admin/AdminService.java
@@ -0,0 +1,21 @@
+package admin;
+
+public class AdminService {
+
+    public AdminService() {
+
+    }
+    public void addAdmin(Admin admin) {
+
+    }
+    public void removeAdmin(Admin admin) {
+
+    }
+    public void updateAdmin(Admin admin) {
+
+    }
+    public Admin getAdmin(String username) {
+        return null;
+    }
+
+}
