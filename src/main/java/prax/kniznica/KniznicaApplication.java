65c4030246343a1eb2e2046a9301316890ab6d69
Richard Dobrovský <richard.dobrovsky@kosickaakademia.sk>
2026-03-19 09:31:53 +0100

    Commiting created project

---
+++ src/main/java/prax/kniznica/KniznicaApplication.java
@@ -0,0 +1,13 @@
+package prax.kniznica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KniznicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KniznicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/prax/kniznica/KniznicaApplicationTests.java
@@ -0,0 +1,13 @@
+package prax.kniznica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KniznicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
