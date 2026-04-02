9056d94f9eda968805b0e95c5c491b149f7b5c43
hejmua <staffdeveloper.discord.com@gmail.com>
2026-04-02 14:20:47 +0200

    rework + fix

--- src/main/java/prax/kniznica/comments/CommentService.java
+++ src/main/java/prax/kniznica/comments/CommentService.java
@@ -1,25 +1,8 @@
 package prax.kniznica.comments;
 
-import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class CommentService {
-
-    private final List<Comment> comments = new ArrayList<>();
-
-    public List<Comment> findAll() {
-        return List.copyOf(comments);
-    }
-
-    public Comment create(Comment comment) {
-        if (comment.getTimestamp() == null) {
-            comment.setTimestamp(Instant.now());
-        }
-        comments.add(comment);
-        return comment;
-    }
+public interface CommentService {
+    List<Comment> findAll();
+    Comment create(Comment comment);
 }
