69bcd00cc32607f42c46096c18dc07b685329c67
Penky777 <lukas.penkert@gmail.com>
2026-05-31 23:27:38 +0200

    Fixed h2 database console

--- src/main/java/prax/kniznica/SecurityConfig.java
+++ src/main/java/prax/kniznica/SecurityConfig.java
@@ -12,11 +12,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
 
+        http
+                .csrf(csrf -> csrf.disable())
+                .headers(headers ->
+                        headers.frameOptions(frame -> frame.disable())
+                )
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll()
+                );
 
         return http.build();
     }
